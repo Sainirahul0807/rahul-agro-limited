049caa6d5c526b06e56d9587d8088800da292e67
Rahul Saini <rahulparshant2004@gmail.com>
2026-09-12 12:33:09 +0530

    Clean up tooltip component to address sourcemap warning

--- artifacts/rahul-agro/src/components/ui/tooltip.tsx
+++ artifacts/rahul-agro/src/components/ui/tooltip.tsx
@@ -5,9 +5,7 @@ import * as TooltipPrimitive from '@radix-ui/react-tooltip';
 import { cn } from '@/lib/utils';
 
 const TooltipProvider = TooltipPrimitive.Provider;
-
 const Tooltip = TooltipPrimitive.Root;
-
 const TooltipTrigger = TooltipPrimitive.Trigger;
 
 const TooltipContent = React.forwardRef<
@@ -26,6 +24,12 @@ const TooltipContent = React.forwardRef<
     />
   </TooltipPrimitive.Portal>
 ));
+
 TooltipContent.displayName = TooltipPrimitive.Content.displayName;
 
-export { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider };
+export {
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+  TooltipProvider,
+};
